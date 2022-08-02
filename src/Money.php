4014bf3e9182666229c59d6d61e43400a3836608
OstrikovG <ostgleal@gmail.com>
2022-08-02 22:56:33 +0300

    Chapter_8_Making_Objects

--- src/Money.php
+++ src/Money.php
@@ -4,9 +4,21 @@ declare(strict_types=1);
 
 namespace OstrikovG\PhpKentBecksTestDrivenDevelopment;
 
-class Money
+abstract class Money
 {
     protected int $amount;
+
+    public abstract function times(int $multiplier): Money;
+
+    public static function dollar(int $amount): Money
+    {
+        return new Dollar($amount);
+    }
+
+    public static function franc(int $amount): Money
+    {
+        return new Franc($amount);
+    }
 
     public function equals(object $object): bool
     {
