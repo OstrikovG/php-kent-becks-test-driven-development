095cf1ec46621340bb760c008acbba738ace6681
OstrikovG <ostgleal@gmail.com>
2022-08-02 22:20:41 +0300

    Chapter_7_Apples_and_Oranges

--- src/Money.php
+++ src/Money.php
@@ -12,6 +12,7 @@ class Money
     {
         /** @var Money $money */
         $money = $object;
-        return $this->amount == $money->amount;
+        return $this->amount == $money->amount
+            && get_class($this) == get_class($object);
     }
 }
